899887733e81a6631216367cf3425ef07223c5c7
fabroche <kraxusmmo@gmail.com>
2023-05-14 16:44:58 -0500

    First Commit

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myImg',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.sass']
+})
+export class ImgComponent {
+
+}
