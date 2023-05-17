213f9a5c155130824bdb62145c993563ebf4918c
fabroche <kraxusmmo@gmail.com>
2023-05-16 22:01:53 -0500

    componente product update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: IProduct[] = [
+    {
+      id: '1',
+      name: 'Bike',
+      price: 100,
+      image: '../assets/products/bike.jpg',
+    },
+    {
+      id: '2',
+      name: 'GoT Collection',
+      price: 180,
+      image: '../assets/products/books.jpg',
+    },
+    {
+      id: '3',
+      name: 'Coffe',
+      price: 20,
+      image: '../assets/products/starbucks1.jpg',
+    },
+  ];
 
   onLoaded(img: string) {
     console.log('log padre');
     console.log('imgUrl :>> ', img);
+  }
+
+  onClickProductImg(img: string) {
+    this.imgParent = img;
   }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { IProduct } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.sass']
+  styleUrls: ['./product.component.sass'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  @Input() product: IProduct = {
+    id: '',
+    name: '',
+    image: '',
+    price: 0,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
